3a42b0d47ebc87a41c92b962a3af28a1fc8c116d
skg1998 <sahilkg1998@gmail.com>
2021-05-28 13:01:40 +0530

    feat(path): create path_module

---
+++ RunTimeJavaScript/app.js
@@ -0,0 +1,19 @@
+//GLOBALS - NO WINDOWS
+
+//__dirname - path to current dirctory
+console.log(__dirname);
+
+//__filename - file name
+console.log(__filename);
+
+//require - function to use modules (common js)
+console.log(require);
+
+//Common.js - every file is a module by default
+//Modules - (Encapsulated code) info about current module file
+require('./os_module');
+
+//process - info about env where the program is being executed
+
+
+
