6a951c1b4dc970b5d643ffe40781043321b3314b
skg1998 <sahilkg1998@gmail.com>
2021-06-02 19:43:08 +0530

    feat(npm): learn about npm

---
+++ RunTimeJavaScript/npm.js
@@ -0,0 +1,12 @@
+//npm Global command, comes with node
+//npm --version
+
+//local dependency - use it only in this particular project
+//npm i <packagename>
+
+//global dependency - use it in any project
+//npm install -g <packagename>
+
+//package.json - mainfest file (store imporant information about project/package)
+//npm init (step by step create)
+//npm init -y  
