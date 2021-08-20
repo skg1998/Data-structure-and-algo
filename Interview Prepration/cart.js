798b4e5976127992c699c62ec125488eec70a286
skg1998 <sahilkg1998@gmail.com>
2021-08-20 21:52:51 +0530

    feat(cart): learn cart functionality

---
+++ Interview Prepration/cart.js
@@ -0,0 +1,17 @@
+//shoping feature
+const user = {
+    name: 'kim',
+    active: true,
+    cart: [],
+    purchase: []
+}
+
+//Implementing a cart
+//Add a cart
+//Add 3% text to item in cart
+// Buy item : purchase
+// Empty cart
+
+//Bonus:
+//aqccept refunds
+//Track user history
