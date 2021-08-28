15f88ba01b73c06197de1993cd2fc8e8080d6c7d
skg1998 <sahilkg1998@gmail.com>
2021-08-28 22:10:54 +0530

    feat(advanced ts ): learn the concept of type interface class subclass generic

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -3,7 +3,7 @@
 /** ***************************************************************************** */
 //type infernce
 /******************************************************************************* */
-let id: number = 5;
+let Id: number = 5;
 let company: string = 'Barozgaar';
 let isPublished: boolean = false;
 let income: any = 50000;
@@ -53,3 +53,78 @@ const user: User = { id: 1, name: 'user' };
 let cid: any = 1;
 let customerId = <number>cid; // way 1
 let productId = cid as number; // way 2
+
+//Function
+
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+function log(message: string | number): void {
+  console.log(message);
+}
+
+//Interface
+interface UserInterface {
+  readonly id: number;
+  name: string;
+  age?: number; // ? make it optional
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'test',
+};
+
+interface mathFunc {
+  (x: number, y: number): number;
+}
+
+const sum: mathFunc = (x: number, y: number): number => x + y;
+const sub: mathFunc = (x: number, y: number): number => x + y;
+
+//classes
+
+interface PersonInterface {
+  id: number;
+  name: string;
+  register(): string;
+}
+class Person implements PersonInterface {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+
+  register() {
+    return `${this.name} is now registered`;
+  }
+}
+
+const test1 = new Person(1, 'test1');
+const test2 = new Person(2, 'test2');
+
+//
+
+class Employee extends Person {
+  position: string;
+
+  constructor(id: number, name: string, position: string) {
+    super(id, name);
+    this.position = position;
+  }
+}
+
+const sew = new Employee(1, 'sew', 'developer');
+
+//Generices
+
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items);
+}
+
+let numArray = getArray<number>([1, 2, 3, 4]);
+let strArray = getArray<string>(['brad', 'test', 'tttt']);
